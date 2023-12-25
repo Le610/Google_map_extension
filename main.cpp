bd771f025c33919938c96cbe9e1287949015182f
Le610 <143785907+Le610@users.noreply.github.com>
2023-12-25 15:50:44 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -26,30 +26,9 @@ const char* password = "";
 const char *apikey = "";
 const String origin = "出發";
 const String destination = "終點";
-/*
-// 定義方向指示
-enum direction
-{
-  left =-1, straight, right
-};
 
-// 定義狀態
-enum driving_mode
-{
-  ontheway=1 , touchdown
-};
-*/
-//定義初始+布林值
-/*
-bool isFetchingData = false;
-bool arrived = false;
-int currentStep = 0;
-int flag=ontheway; //1:ontheway,2: touchdown next step
-int total_dirvalue = 0;
-*/
 // JSON 解析器
-//String jsonStr = ""; // 保存完整的JSON字符串
-//HTTPClient http; // 在这里声明 HTTPClient 对象
+
 DynamicJsonDocument doc(16384); // JSON解析的缓冲区
 
 // 函數原型
@@ -63,63 +42,8 @@ int precheck_dirvalue(JsonObject step, int *countR, int *countL,int *countRR);
 String extractRoadName(JsonObject step);
 void processStep(JsonObject step, int j, int total_dirvalue);
 int check_position(JsonObject step, int total, int flag, JsonArray legs, int currentStep, int *dis);
-/*
-void LCDupdate(){
-  tft.begin(); // Initialize TFT
-  tft.setRotation(1); // Set screen rotation 90
-  tft.fillScreen(TFT_BLACK); // Clear the screen
-  u8f.begin(tft); // Connect u8g2 to TFT_eSPI
-  u8f.setFontMode(0);
-  u8f.setFontDirection(0);
-  u8f.setFont(u8g2_font_unifont_t_chinese1);  
-}
-*/
-/*
-void drawText(const char* text, uint16_t color, uint8_t size, bool wrap) {
-tft.setTextColor(color);
-tft.setTextSize(size);
-tft.setTextWrap(wrap);
-tft.print(text);
-}
 
-String htmlEntitiesDecode(String input) {
-  String output = input;
-  output.replace("&quot;", "\"");
-  output.replace("&amp;", "&");
-  output.replace("&lt;", "<");
-  output.replace("&gt;", ">");
-  return output;
-}
 
-String removeHTMLTags(String input) {
-  int start = input.indexOf('<');
-  int end = input.indexOf('>', start);
-  while (start >= 0 && end >= 0) {
-    input.remove(start, end - start + 1);
-    start = input.indexOf('<');
-    end = input.indexOf('>', start);
-  }
-  return input;
-}
-
-int sim_position(int flag, int currentStep) {
-if (flag==ontheway){
-flag=touchdown;
-Serial.println("行進中");
-Serial.print("準備切換為flag:");
-Serial.println(flag);
-}
-else if (flag==touchdown){
-Serial.print("抵達路線,currenstep=");
-Serial.println(currentStep);
-//currentStep++;
-flag=ontheway;
-Serial.print("準備切換為flag:");
-Serial.println(flag);
-}
-return flag;
-}
-*/
 void setup() {
   LCDupdate();
   Serial.begin(115200);
